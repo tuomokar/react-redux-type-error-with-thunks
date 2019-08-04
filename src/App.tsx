10d33a88f1f83982c2c7fd308d7b2fcb3f0339e5
Tuomo Oila <tuomo.oila@haahtela.fi>
2019-08-04 12:43:22 +0300

    App as a typescript file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { AnyAction } from "redux";
 
 function App() {
   return (
